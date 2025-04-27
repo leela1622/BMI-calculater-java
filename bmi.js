2668b3195c2addb51d319780b156904e7bf09ae1
leela1622 <leelaleelav7@gmail.com>
2025-04-27 00:16:25 -0700

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,26 @@
+function bmi()
+{
+    var w=parseInt(prompt("Your Weight"))
+    var h=parseInt(prompt("Your Height"))
+    var bmi=(w/((h/100) **2))
+    document.write("your BMI is:"+bmi)
+
+
+if(bmi<18.5)
+{
+    alert("Thinness")
+}
+else if((bmi>18.5)&&(bmi<=25))
+{
+    alert("Normal")
+}
+else if((bmi>25)&&(bmi<=30))
+    {
+        alert("Over Weight")
+    }
+    else if((bmi>30) && (bmi<=35))
+    {
+        alert("Obese")
+    }
+}
+bmi();
